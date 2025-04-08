c3af3fd795974d95a09b7cf5346883ddc5c8e84c
amnesia2k <tilewaolatoye17@gmail.com>
2025-04-08 22:27:59 +0100

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+import http from "http";
+
+const PORT = process.env.PORT || 5000;
+
+const server = http.createServer((req, res) => {
+  res.writeHead(200, { "Content-Type": "text/html" });
+  res.end("<h1>Hello World</h1>");
+});
+
+// console.log(document);
+
+server.listen(PORT, () => {
+  console.log(`Server is running on port http://localhost:${PORT}`);
+});
