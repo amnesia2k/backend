316dfa4f36d77688d8c50a858fc0d66bbfa325c9
amnesia2k <tilewaolatoye17@gmail.com>
2025-04-09 23:15:48 +0100

    init

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const server = http.createServer((req, res) => {
   res.end("<h1>Hello World</h1>");
 });
 
-console.log(document);
+// console.log(document);
 
 server.listen(PORT, () => {
   console.log(`Server is running on port http://localhost:${PORT}`);
